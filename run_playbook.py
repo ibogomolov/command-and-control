2e762f4412b8390ada0e4ab86db5e69be37eb62c
Bogomolov, Igor <igor.bogomolov@gmail.com>
2025-11-04 21:26:52 +0100

    feat: check for the correct number of arguments

--- run_playbook.py
+++ run_playbook.py
@@ -4,6 +4,10 @@ from program.executor import PlaybookExecutor
 from program.playbook import Playbook
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <playbook>')
+        sys.exit(1)
+
     playbook = Playbook.from_file(sys.argv[1])
     executor = PlaybookExecutor(playbook)
     executor.execute()
